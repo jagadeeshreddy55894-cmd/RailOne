a8147f58777166a1ff93a45a491dfbf251d8db3a
jagadeesh reddy <jagadeeshreddy55894@gmail.com>
2026-07-23 14:14:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-document.addEventListener('gesturestart', function (e) {
-    e.preventDefault();
-});
-
-document.addEventListener('dblclick', function (e) {
-    e.preventDefault();
-});
+console.log("RailOne Loaded");
